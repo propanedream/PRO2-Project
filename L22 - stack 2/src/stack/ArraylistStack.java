62b8eaee16c8b57ae7fb53054ee891893643fd0d
propanedream <eaaadey@students.eaaa.dk>
2022-11-28 20:21:17 +0100

    opdatering af hele mappen

---
+++ L22 - stack 2/src/stack/ArraylistStack.java
@@ -0,0 +1,30 @@
+package stack;
+
+public class ArraylistStack implements StackI{
+
+
+    @Override
+    public void push(Object element) {
+
+    }
+;
+    @Override
+    public Object pop() {
+        return null;
+    }
+
+    @Override
+    public Object peek() {
+        return null;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+}
